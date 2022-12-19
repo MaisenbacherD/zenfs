3f0581d5387f8192f2d9d35f81c2a32f4fda1b00
Dennis Maisenbacher <dennis.maisenbacher@wdc.com>
2022-12-19 11:44:41 +0100

    TMP: Use the existing db for the added tests
    Signed-off-by: Dennis Maisenbacher <dennis.maisenbacher@wdc.com>

--- tests/quick_performance/0002_readseq.sh
+++ tests/quick_performance/0002_readseq.sh
@@ -6,7 +6,7 @@ echo "" > $TEST_OUT
 
 for VALUE_SIZE in 100 200 400 1000 2000 8000; do
   NUM=$(( $WORKLOAD_SZ / $VALUE_SIZE ))
-  DB_BENCH_PARAMS="--benchmarks=readseq --num=$NUM --value_size=$VALUE_SIZE --compression_type=none --histogram $FS_PARAMS $DB_BENCH_EXTRA_PARAMS"
+  DB_BENCH_PARAMS="--benchmarks=readseq --use_existing_db --num=$NUM --value_size=$VALUE_SIZE --compression_type=none --histogram $FS_PARAMS $DB_BENCH_EXTRA_PARAMS"
   echo "# Running db_bench with parameters: $DB_BENCH_PARAMS" >> $TEST_OUT
   $TOOLS_DIR/db_bench $DB_BENCH_PARAMS >> $TEST_OUT
   RES=$?
